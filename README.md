88b09d311d541bbdd8b2a9e7636ed42dcf6d06d7
TeraProxy <siebenschwerter@aol.com>
2017-08-10 15:59:54 +0200

    Bug fix on 0 flight energy

--- README.md
+++ README.md
@@ -15,6 +15,18 @@ Any other input returns a summary of above commands in the game.
 Alternative commands in all other chats:  
 * !fly - Toggles between "on" and "off" state  
   
+Support for Pinkie Pie's command module:  
+* /proxy fly - Toggles between "on" and "off" state  
+  
 ## Safety
 Whatever you send to "!UnlimitedFlight" in game is intercepted client-side. The chat is NOT sent to the server.  
-This is obviously one of the more questionable modules, so I do not recommend using it around players you don't trust.
+This is obviously one of the more questionable modules, so I do not recommend using it around players you don't trust.  
+  
+## Changelog
+### 1.2.0
+* [*] Fixed a bug when running out of energy. Thanks to Pinkie Pie
+* [+] Added support for Pinkie Pie's command module which is now a requirement
+### 1.1.0
+* [+] Added !fly command to toggle between "on" and "off" in non-whisper chats
+### 1.0.0
+* [*] Initial Release
--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+const Command = require('command')
+
 module.exports = function UnlimitedFlight(dispatch) {
 	
     let enabled = false,
+		energy = 0,
 		cid = null,
 		player = ''
+	
+	dispatch.hook('S_LOGIN', 1, event => { 
+		({cid} = event) 
+		player = event.name
+	})
 	
 	dispatch.hook('S_CANT_FLY_ANYMORE', 1, (event) => {
 		if(enabled) return false
     })
 	
-	dispatch.hook('S_LOGIN', 1, event => { 
-		({cid} = event) 
-		player = event.name
+	dispatch.hook('S_PLAYER_CHANGE_FLIGHT_ENERGY', 1, event => { 
+		energy = event.energy
+	})
+	
+	dispatch.hook('C_PLAYER_FLYING_LOCATION', 3, event => { // Thanks to Pinkie Pie
+		if(energy == 0 && event.type !== 7 && event.type !== 8) {
+			event.type = 7
+			return true
+		}
 	})
 	
 	// ################# //
@@ -60,6 +74,20 @@ module.exports = function UnlimitedFlight(dispatch) {
 				console.log('Unlimited Flight disabled.')
 			}
 			return false
+		}
+	})
+	
+	const command = Command(dispatch)
+	command.add('fly', function() {
+		if(!enabled) {
+			enabled = true
+			message('Unlimited Flight <font color="#56B4E9">enabled</font>.')
+			console.log('Unlimited Flight enabled.')
+		}
+		else {
+			enabled = false
+			message('Unlimited Flight <font color="#E69F00">disabled</font>.')
+			console.log('Unlimited Flight disabled.')
 		}
 	})
 }
