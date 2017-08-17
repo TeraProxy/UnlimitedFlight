fc858cfaa1f7f68f18dfeb1c7799c916a03d9eb1
TeraProxy <siebenschwerter@aol.com>
2017-08-17 15:17:05 +0200

    Conversion to command, cleanup

--- README.md
+++ README.md
@@ -1,28 +1,21 @@
 # UnlimitedFlight 
 A tera-proxy module that lets you fly endlessly.  
-Someone else released his own version of this mod, so I feel okay about releasing it now.  
   
 ## Usage  
-While in game, open a whisper chat session with "!UnlimitedFlight" by typing "/w !unlimitedflight" in chat and hitting the space bar.
-This serves as the script's command interface. 
+While in game, open a proxy chat session by typing "/proxy" or "/8" in chat and hitting the space bar.  
+This serves as the script's command interface.  
 The following commands are supported:  
   
-* on - Enables the script  
-* off - Disables the script (default)  
-  
-Any other input returns a summary of above commands in the game.  
-  
-Alternative commands in all other chats:  
-* !fly - Toggles between "on" and "off" state  
-  
-Support for Pinkie Pie's command module:  
-* /proxy fly - Toggles between "on" and "off" state  
+* fly - enable/disable UnlimitedFlight  
   
 ## Safety
-Whatever you send to "!UnlimitedFlight" in game is intercepted client-side. The chat is NOT sent to the server.  
+Whatever you send to the proxy chat in game is intercepted client-side. The chat is NOT sent to the server.  
 This is obviously one of the more questionable modules, so I do not recommend using it around players you don't trust.  
   
 ## Changelog
+### 1.2.1
+* [*] Some code cleanup
+* [*] Full conversion to Pinkie Pie's command module
 ### 1.2.0
 * [*] Fixed a bug when running out of energy. Thanks to Pinkie Pie
 * [+] Added support for Pinkie Pie's command module which is now a requirement
--- index.js
+++ index.js
@@ -3,14 +3,7 @@ const Command = require('command')
 module.exports = function UnlimitedFlight(dispatch) {
 	
     let enabled = false,
-		energy = 0,
-		cid = null,
-		player = ''
-	
-	dispatch.hook('S_LOGIN', 1, event => { 
-		({cid} = event) 
-		player = event.name
-	})
+		energy = 0
 	
 	dispatch.hook('S_CANT_FLY_ANYMORE', 1, (event) => {
 		if(enabled) return false
@@ -31,63 +24,10 @@ module.exports = function UnlimitedFlight(dispatch) {
 	// ### Chat Hook ### //
 	// ################# //
 	
-	dispatch.hook('C_WHISPER', 1, (event) => {
-		if(event.target.toUpperCase() === "!unlimitedflight".toUpperCase()) {
-			if (/^<FONT>on?<\/FONT>$/i.test(event.message)) { 
-				enabled = true
-				message('Unlimited Flight <font color="#56B4E9">enabled</font>.')
-			}
-			else if (/^<FONT>off?<\/FONT>$/i.test(event.message)) {
-				enabled = false
-				message('Unlimited Flight <font color="#E69F00">disabled</font>.')
-			}
-			else message('Commands:<br>'
-								+ ' "on" (enable Unlimited Flight),<br>'
-								+ ' "off" (disable Unlimited Flight)'
-						)
-			return false
-		}
-	})
-	
-	function message(msg) {
-		dispatch.toClient('S_WHISPER', 1, {
-			player: cid,
-			unk1: 0,
-			gm: 0,
-			unk2: 0,
-			author: '!UnlimitedFlight',
-			recipient: player,
-			message: msg
-		})
-	}
-	
-	dispatch.hook('C_CHAT', 1, event => {
-		if(/^<FONT>!fly<\/FONT>$/i.test(event.message)) {
-			if(!enabled) {
-				enabled = true
-				message('Unlimited Flight <font color="#56B4E9">enabled</font>.')
-				console.log('Unlimited Flight enabled.')
-			}
-			else {
-				enabled = false
-				message('Unlimited Flight <font color="#E69F00">disabled</font>.')
-				console.log('Unlimited Flight disabled.')
-			}
-			return false
-		}
-	})
-	
 	const command = Command(dispatch)
-	command.add('fly', function() {
-		if(!enabled) {
-			enabled = true
-			message('Unlimited Flight <font color="#56B4E9">enabled</font>.')
-			console.log('Unlimited Flight enabled.')
-		}
-		else {
-			enabled = false
-			message('Unlimited Flight <font color="#E69F00">disabled</font>.')
-			console.log('Unlimited Flight disabled.')
-		}
+	command.add('fly', () => {
+		enabled = !enabled
+		command.message('[Unlimited Flight] ' + (enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
+		console.log('[Unlimited Flight] ' + (enabled ? 'enabled' : 'disabled'))
 	})
 }
